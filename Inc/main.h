2e224308c23a38d356bfb41ef68bad80643e90bc
buritomo <42825048+buritomo@users.noreply.github.com>
2020-04-28 00:58:11 +0900

    Update main.h

--- Inc/main.h
+++ Inc/main.h
@@ -62,8 +62,6 @@ void Error_Handler(void);
 #define MCO_GPIO_Port GPIOF
 #define VCP_TX_Pin GPIO_PIN_2
 #define VCP_TX_GPIO_Port GPIOA
-#define LED1_Pin GPIO_PIN_10
-#define LED1_GPIO_Port GPIOA
 #define SWDIO_Pin GPIO_PIN_13
 #define SWDIO_GPIO_Port GPIOA
 #define SWCLK_Pin GPIO_PIN_14
